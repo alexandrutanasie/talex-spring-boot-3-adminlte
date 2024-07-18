ce69aac253437aad2d060f308c1cdf1802a9e03e
Alexandru Tanasie <alexandru.tanasie@playmore.ro>
2024-07-18 14:59:43 +0300

    spring initialzr

---
+++ src/main/java/talex/adminlte/AdminlteApplication.java
@@ -0,0 +1,13 @@
+package talex.adminlte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminlteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminlteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/talex/adminlte/AdminlteApplicationTests.java
@@ -0,0 +1,13 @@
+package talex.adminlte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminlteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
