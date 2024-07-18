dab581328ddbc64a2fdb19fcfad8736f2e235999
Alexandru Tanasie <alexandrutanasiee@gmail.com>
2024-07-18 15:28:08 +0300

    create routes with templates

---
+++ src/main/java/talex/adminlte/controllers/AuthController.java
@@ -0,0 +1,12 @@
+package talex.adminlte.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
+    }    
+}
